dd0089221667cd0970041f00969e9d678437f4e6
WHORYZEN <kanishkshkl@gmail.com>
2024-06-29 20:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str: string, subStr: string): number {
+    // If either string is empty, or subStr is longer than str, return -1
+    if (str.length === 0 || subStr.length === 0 || subStr.length > str.length) {
+        return -1;
+    }
+
+    // Convert both strings to lowercase for case-insensitive comparison
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+
+    // Perform case-insensitive search
+    return lowerStr.indexOf(lowerSubStr);
 }
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
