792648dfc7b4c76624db35dfe292478d70f50063
WHORYZEN <kanishkshkl@gmail.com>
2024-06-29 20:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function indexOfIgnoreCase(str: string, subStr: string): number {
-    // If either string is empty, or subStr is longer than str, return -1
+
     if (str.length === 0 || subStr.length === 0 || subStr.length > str.length) {
         return -1;
     }
 
-    // Convert both strings to lowercase for case-insensitive comparison
     const lowerStr = str.toLowerCase();
     const lowerSubStr = subStr.toLowerCase();
 
-    // Perform case-insensitive search
     return lowerStr.indexOf(lowerSubStr);
 }
 
